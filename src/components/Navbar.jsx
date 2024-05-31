db8e9b0e29547ea8bd54a39a3e804e665dc6bab8
fullstackkg <khalilhgayle@gmail.com>
2024-05-31 01:10:55 -0400

    Add initial Navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "../styles/Navbar.css";
+
+function Navbar() {
+  return (
+    <nav>
+      <div className="nav-center">
+        <ul>
+          <li>
+            <a href="#home">Home</a>
+          </li>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+          <li>
+            <a href="#services">Services</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
